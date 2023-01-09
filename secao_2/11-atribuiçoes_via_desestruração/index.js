8ffc2c5764cb2d424f9328c94de1d8986d1c626c
Santzu-27 <gabi260402@gmail.com>
2023-01-09 14:33:53 -0300

    Create index.js

---
+++ secao_2/11-atribuiçoes_via_desestruração/index.js
@@ -0,0 +1,13 @@
+let a = 'A'
+let b = 'B'
+let c = 'C'
+
+const letras = [b, c, a];
+[a, b, c] = letras
+
+console.log('1 -', a, b, c)
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const [primeiroNumero, segundoNumero] = numeros;
+
+console.log('2 -', primeiroNumero, segundoNumero);
