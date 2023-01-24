eb547d0d4bc4e256dad67171b8de4609abda3672
Santzu-27 <gabi260402@gmail.com>
2023-01-24 09:16:25 -0300

    timer

---
+++ exercicios/3-timer/correcao/modelo/assets/js/main.js
@@ -0,0 +1,14 @@
+const relogio = document.querySelector('#relogio');
+const iniciar = document.querySelector('#iniciar');
+const pausar = document.querySelector('#pausar');
+const zerar = document.querySelector('#zerar');
+
+iniciar.addEventListener('click', function(e){
+
+})
+pausar.addEventListener('click', function(e){
+
+})
+zerar.addEventListener('click', function(e){
+
+})
