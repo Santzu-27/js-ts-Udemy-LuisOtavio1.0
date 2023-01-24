36d39ed1f4a3e3afc270fafe255a35c4bc82834c
Santzu-27 <gabi260402@gmail.com>
2023-01-24 17:21:26 -0300

    timer

--- exercicios/3-timer/correcao/modelo/assets/js/main.js
+++ exercicios/3-timer/correcao/modelo/assets/js/main.js
@@ -1,14 +1,42 @@
-const relogio = document.querySelector('#relogio');
-const iniciar = document.querySelector('#iniciar');
-const pausar = document.querySelector('#pausar');
-const zerar = document.querySelector('#zerar');
+function relogio(){
+    const relogio = document.querySelector('.relogio');
+    let segundos = 0;
+    let timer;
 
-iniciar.addEventListener('click', function(e){
+    document.addEventListener('click', function(e){
+        const el = e.target;
+        if (el.classList.contains('zerar')){
+            clearInterval(timer)
+            segundos = 0;
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado')
+        }
 
-})
-pausar.addEventListener('click', function(e){
+        if (el.classList.contains('pausar')){
+            clearInterval(timer)
+            relogio.classList.add('pausado')
+        }
 
-})
-zerar.addEventListener('click', function(e){
+        if (el.classList.contains('iniciar')){
+            clearInterval(timer);
+            iniciaRelogio();
+            relogio.classList.remove('pausado')
+        }
+    })
 
-})
+    function criaSegundos(segundos){
+        const date = new Date(segundos * 1000);
+        return date.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+
+    function iniciaRelogio(){
+        timer = setInterval(() => {
+            segundos++;
+            relogio.innerHTML = criaSegundos(segundos)
+        }, 1000);
+    }
+}
+relogio()
