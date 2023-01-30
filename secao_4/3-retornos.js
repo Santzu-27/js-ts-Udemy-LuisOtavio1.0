d4db37c4e1f426338a303e8cdb80c9ee41b11c17
Santzu-27 <gabi260402@gmail.com>
2023-01-30 17:24:38 -0300

    Create 3-retornos.js

---
+++ secao_4/3-retornos.js
@@ -0,0 +1,24 @@
+function falaFrase(comeco){
+    function falaResto(resto){
+        return comeco + ' ' + resto;
+    }
+    return falaResto
+}
+
+const chamado = falaFrase('Olá')
+console.log(chamado('Mundo'))
+console.log('=============================================================')
+
+function criaMultiplicador(multiplicador){
+    return function(n){
+        return n * multiplicador;
+    }
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(10));
+console.log(triplica(10));
+console.log(quadriplica(10));
