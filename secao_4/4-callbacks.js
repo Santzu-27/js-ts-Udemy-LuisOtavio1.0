06db57f370e0ebc610f3bd76b13186b311c485ff
Santzu-27 <gabi260402@gmail.com>
2023-06-28 16:12:21 -0300

    Update 4-callbacks.js

--- secao_4/4-callbacks.js
+++ secao_4/4-callbacks.js
@@ -1,1 +1,53 @@
-function rand(min = 1000, max = 3000){}
+function rand(min = 1000, max = 3000){
+    const num = Math.random() *(max - min) + min;
+    return Math.floor(num);
+}
+
+function f1(callback){
+    setTimeout(function(){
+        console.log('f1');
+        if (callback) callback();
+    }, rand());
+}
+function f2(callback){
+    setTimeout(function(){
+        console.log('f2');
+        if (callback) callback();
+    }, rand());
+}
+function f3(callback){
+    setTimeout(function(){
+        console.log('f3');
+        if (callback) callback();
+    }, rand());
+}
+
+console.log(rand());
+
+// f1(function(){
+//     f2(function(){
+//         f3(function(){
+//             console.log('Ola cambanda');
+//         })
+//     });
+// });
+// É a mesma coisa que f1(() => f2(() => f3(() => console.log('Olá Mundo!'))))
+
+
+
+f1(f1Callback);
+
+
+function f1Callback(){
+    f2(f2Callback);
+}
+
+function f2Callback(){
+    f3(f3Callback);
+}
+
+function f3Callback(){
+    console.log('Ola cambanda');
+}
+
+f1(() => f2(() => f3(() => console.log('Olá Mundo!'))));
