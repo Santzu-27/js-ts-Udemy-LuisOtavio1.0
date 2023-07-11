3dc535e01f5735fec05403eab52a7b635f8e6cd3
Santzu-27 <gabi260402@gmail.com>
2023-07-11 18:03:03 -0300

    Update 5-IIFE.js

--- secao_4/5-IIFE.js
+++ secao_4/5-IIFE.js
@@ -1,1 +1,18 @@
-//Funções Imediatas
+//Funções Imediatas -> Immediatly Invoked Function Expression
+const myName = 'Test';
+
+(function(){
+    const lastName = 'Santos';
+
+    function createName(myName){
+        return myName + ' ' + lastName;
+    }
+
+    function sayName(){
+        console.log(createName('Gabriel'))
+    }
+
+    sayName();
+})();
+console.log();
+// console.log(myName);
