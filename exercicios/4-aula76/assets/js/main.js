50e73a80e3fd41def430121b5cab426d12b3fbb5
Santzu-27 <gabi260402@gmail.com>
2023-08-23 13:36:04 -0300

    comit

--- exercicios/4-aula76/assets/js/main.js
+++ exercicios/4-aula76/assets/js/main.js
@@ -1,6 +1,42 @@
 function createCalculator(){
-    
     return{
+        	display: document.querySelector('.display'),
 
+            start(){
+                this.btnsClick();
+            },
+            
+            clearDisplay(){
+                this.display.value = '';
+            },
+
+            delOne(){
+                this.display.value = this.display.value.slice(0, -1);
+            },
+
+            btnsClick(){
+                document.addEventListener('click', (e) => {
+                    const el = e.target;
+                    
+                    if(el.classList.contains('btn-num')){
+                        this.btnDisplay(el.innerText);
+                    }
+                    
+                    if(el.classList.contains('btn-clear')){
+                        this.clearDisplay();
+                    }
+
+                    if(el.classList.contains('btn-del')){
+                        this.delOne();
+                    }
+                });
+            },
+
+            btnDisplay(valor){
+                this.display.value += valor;
+            }
     }
 }
+
+const calculadora = createCalculator();
+calculadora.start();
