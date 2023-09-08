b4c64d405a40962f70dcdea853ab6065f274da08
Santzu-27 <gabi260402@gmail.com>
2023-09-08 20:42:23 -0300

    Update main.js

--- exercicios/5-rebuild_constructors/assets/js/main.js
+++ exercicios/5-rebuild_constructors/assets/js/main.js
@@ -1,72 +1,82 @@
-function createCalculator(){
-    return{
-        	display: document.querySelector('.display'),
+// function createCalculator(){
+//     return{
+//         	display: document.querySelector('.display'),
 
-            start(){
-                this.btnsClick();
-                this.pressEnter();
-            },
+//             start(){
+//                 this.btnsClick();
+//                 this.pressEnter();
+//             },
 
-            doMath(){
-                let count = this.display.value;
-                try{
-                    count = eval(count);
-                    if(Number.isNaN(count)){
-                        alert('Conta Inválida')
-                        return;
-                    }
+//             doMath(){
+//                 let count = this.display.value;
+//                 try{
+//                     count = eval(count);
+//                     if(Number.isNaN(count)){
+//                         alert('Conta Inválida')
+//                         return;
+//                     }
                     
-                    this.display.value = count;
-                }catch(e){
-                    alert('Conta Inválida')
-                }
-            },
+//                     this.display.value = count;
+//                 }catch(e){
+//                     alert('Conta Inválida')
+//                 }
+//             },
             
-            pressEnter(){
-                this.display.addEventListener('keydown', e =>{
-                    if(e.keyCode === 13){
-                        this.doMath();
-                    }
-                })
-            },
+//             pressEnter(){
+//                 this.display.addEventListener('keydown', e =>{
+//                     if(e.keyCode === 13){
+//                         this.doMath();
+//                     }
+//                 })
+//             },
 
-            clearDisplay(){
-                this.display.value = '';
-            },
+//             clearDisplay(){
+//                 this.display.value = '';
+//             },
 
-            delOne(){
-                this.display.value = this.display.value.slice(0, -1);
-            },
+//             delOne(){
+//                 this.display.value = this.display.value.slice(0, -1);
+//             },
 
 
-            btnsClick(){
-                document.addEventListener('click', (e) => {
-                    const el = e.target;
+//             btnsClick(){
+//                 document.addEventListener('click', (e) => {
+//                     const el = e.target;
                     
-                    if(el.classList.contains('btn-num')){
-                        this.btnDisplay(el.innerText);
-                    }
+//                     if(el.classList.contains('btn-num')){
+//                         this.btnDisplay(el.innerText);
+//                     }
                     
-                    if(el.classList.contains('btn-clear')){
-                        this.clearDisplay();
-                    }
+//                     if(el.classList.contains('btn-clear')){
+//                         this.clearDisplay();
+//                     }
 
-                    if(el.classList.contains('btn-del')){
-                        this.delOne();
-                    }
-                    if(el.classList.contains('btn-eq')){
-                        this.doMath();
-                    }
+//                     if(el.classList.contains('btn-del')){
+//                         this.delOne();
+//                     }
+//                     if(el.classList.contains('btn-eq')){
+//                         this.doMath();
+//                     }
 
-                    this.display.focus();
-                });
-            },
+//                     this.display.focus();
+//                 });
+//             },
 
-            btnDisplay(valor){
-                this.display.value += valor;
-            }
+//             btnDisplay(valor){
+//                 this.display.value += valor;
+//             }
+//     }
+// }
+
+// const calculadora = createCalculator();
+// calculadora.start();
+
+function Calculator(){
+    const display = document.querySelector('.display');
+
+    this.start = (e) => {
+
     }
 }
 
-const calculadora = createCalculator();
-calculadora.start();
+const calculator = new Calculator();
