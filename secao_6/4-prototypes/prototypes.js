634faa84a8b319c99800e0120c205345cc339f14
Gabriel <gabi260402@gmail.com>
2024-02-13 11:44:59 -0300

    Prototypes

--- secao_6/4-prototypes/prototypes.js
+++ secao_6/4-prototypes/prototypes.js
@@ -5,7 +5,7 @@ function Person(name, lastName){
 
 Person.prototype.fullName = function(){
     return this.name + ' aaaa ' + this.lastName;
-} 
+} //São criados apenas uma vez. Quando o metodo é colocado dentro do objeto se é criado várias vezes 
 
 const p1 = new Person("Laís", "Paim");
 console.log(p1.fullName())
