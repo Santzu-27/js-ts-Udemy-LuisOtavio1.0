88295b6324894c6e95ce383722156af9d9b34074
Gabriel <gabi260402@gmail.com>
2024-02-29 10:09:30 -0300

    Update objectsteste.js

--- exercicios/0-meus/objectsteste.js
+++ exercicios/0-meus/objectsteste.js
@@ -2,4 +2,18 @@ const Pessoas = [];
 
 function PessoaConstructor(){
     
+}///
+
+function factory(variavel){
+    return{
+        variavel: variavel,
+        outraVari: variavel + 'etc',
+        funcao: function(){
+            console.log(this.outraVari);
+        }
+    }
 }
+
+const fac = factory('abc ');
+fac.funcao()
+//A saída: abc etc
