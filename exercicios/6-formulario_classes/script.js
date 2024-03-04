722afc5ceb6f7465440263f611ccc665c8e2d4e6
Gabriel <gabi260402@gmail.com>
2024-03-04 07:10:37 -0300

    forms

---
+++ exercicios/6-formulario_classes/script.js
@@ -0,0 +1,36 @@
+class Form{
+    constructor(){
+        this.form = document.querySelector('#form')
+        this.nameInput = document.querySelector('#nome');
+        this.lastnameInput = document.querySelector('#sobrenome');
+        this.cpfInput = document.querySelector('#CPF');
+        this.userInput = document.querySelector('#usuario');
+        this.passInput = document.querySelector('#senha');
+        this.passConfInput = document.querySelector('#rep-senha');
+        this.form.addEventListener('submit', e => {
+            this.handleSubmit(e);
+        })
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        this.verify();
+    }
+    verify(){
+        let valid = false;
+        for(let field of this.form){
+            if(!field.value && field.tagName.toLowerCase() == 'input'){
+                this.createError(field, field.id);
+            }
+        }
+    }
+    createError(field, id){
+        const div = document.createElement('div');
+        div.innerHTML = `Campo ${id} deve ser preenchido.`;
+        div.classList.add('error-text');
+        field.insertAdjacentElement('afterend', div)
+    }
+}
+
+
+const form1 = new Form();;
+form1.func()
