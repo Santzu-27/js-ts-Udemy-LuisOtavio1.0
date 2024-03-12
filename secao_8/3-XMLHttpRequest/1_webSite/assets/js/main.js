311b5c022b8fe446d4a67d263f8e9b3a2e9b40ea
Gabriel <gabi260402@gmail.com>
2024-03-12 18:10:06 -0300

    xmlh

--- secao_8/3-XMLHttpRequest/1_webSite/assets/js/main.js
+++ secao_8/3-XMLHttpRequest/1_webSite/assets/js/main.js
@@ -1,26 +1,41 @@
 const request = obj => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true);
-    xhr.send(null); //Pois é um get
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send(null); //Pois é um get
 
-    xhr.addEventListener('load', () => {
-        if (xhr.status >= 200 && xhr.status < 300 ){
-            obj.success(xhr.responseText);
-        }else{
-            obj.error({
-                code: xhr.status,
-                msg: xhr.statusText
-            });
-        }
-    });
+        xhr.addEventListener('load', () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+            }
+        });
+    })
 };
 
-document.addEventListener('click', e =>{
-    const e = e.target;
+document.addEventListener('click', e => {
+    const el = e.target;
     const tag = el.tagName.toLowerCase();
-
-    if(tag === 'a'){
-        loadPage(e);
+    console.log(tag)
+    if (tag === 'a') {
+        e.preventDefault();
+        loadPage(el);
     }
 })
-
+async function loadPage(e) {
+    const href = e.getAttribute('href');
+    const response = await request({
+        method: 'GET',
+        url: href
+    })
+    try {
+        loadResult(response)
+    } catch (error) {
+        console.log('Erro: ' + error);
+    }
+}
+function loadResult(response) {
+    const result = document.querySelector('#result');
+    result.innerHTML = response;
+}
