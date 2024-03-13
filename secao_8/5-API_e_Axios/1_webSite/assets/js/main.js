4d3db142ab0766c032af442e4d4e1460bba45b6c
Gabriel <gabi260402@gmail.com>
2024-03-13 18:05:58 -0300

    att

--- secao_8/5-API_e_Axios/1_webSite/assets/js/main.js
+++ secao_8/5-API_e_Axios/1_webSite/assets/js/main.js
@@ -3,5 +3,27 @@ fetch('assets/json/pessoas.json')
   .then(json => loadElementsInPage(json))
 
 function loadElementsInPage(json){
+  const table = document.createElement('table');
+  let thName = document.createElement('th');
+  let thAge = document.createElement('th');
+  let thSalary = document.createElement('th');
+  let trHead = document.createElement('tr')
+
+  thName.innerHTML = 'Nome';
+  thAge.innerHTML = 'Idade'
+  thSalary.innerHTML = 'Salário'
+
+  trHead.appendChild(thName);
+  trHead.appendChild(thAge);
+  trHead.appendChild(thSalary);
+  table.appendChild(trHead);
   
+  for (const person of json) {
+    let tr = document.createElement('tr')
+    let td = document.createElement('td')
+    
+  }
+  const result = document.querySelector('#result')
+  result.appendChild(table);
 }
+// loadElementsInPage()
